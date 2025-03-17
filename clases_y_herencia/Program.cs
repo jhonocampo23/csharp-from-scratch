f40bbfcaee93de013ca3c07b9c7f868ca7cd16ce
unknown <jhonj2310@gmail.com>
2025-03-17 11:49:49 -0500

    Add new clases y herencia project

---
+++ clases_y_herencia/Program.cs
@@ -0,0 +1,34 @@
+﻿// See https://aka.ms/new-console-template for more information
+/* Clases */
+// var rect = new Rectangulo();
+// rect.Base = 20;
+// rect.Altura = 40;
+// Console.WriteLine($"El área es: {rect.Area()}");
+// Console.WriteLine($"El perímetro es: {rect.Perimetro()}");
+/* End Clases */
+
+var cuadr = new Cuadrado();
+cuadr.Base = 20;
+cuadr.Altura = 40;
+Console.WriteLine($"El área es: {cuadr.Area()}");
+Console.WriteLine($"El perímetro es: {cuadr.Perimetro()}");
+Console.WriteLine($"El lado es: {cuadr.Lado}");
+
+class Rectangulo
+{
+    public double Base { get; set; }
+
+    public double Altura { get; set; }
+    public double Area() => Base * Altura;
+    public double Perimetro() => 2 * Base + 2 * Altura;
+}
+
+class Cuadrado : Rectangulo
+{
+    public double Lado
+    {
+        set { Base = value; Altura = value; }
+        get { return Base; }
+
+    }
+}
