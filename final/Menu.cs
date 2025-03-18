07ed9f1d3fa9bd5df8406ac3c7aec757f5946496
unknown <jhonj2310@gmail.com>
2025-03-17 23:49:39 -0500

    Add Menu text

---
+++ final/Menu.cs
@@ -0,0 +1,13 @@
+namespace itm.csharp.basic
+{
+    public static class Menu
+    {
+        public static void MostrarMenu()
+        {
+            Console.WriteLine("1. Determinar si un úmero es positivo, negativo o cero.");
+            Console.WriteLine("2. Operaciones básicas entre dos números.");
+            Console.WriteLine("3. Calcular área y perímetro de un rectángulo.");
+            Console.WriteLine("4. Determinar si un año es bisiesto.");
+        }
+    }
+}
